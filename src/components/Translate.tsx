7cd425fcfa60608644c36e57de5a868ebe6ad6c7
minhbui1510 <buihaiminh.2k@gmail.com>
2025-05-15 00:52:27 +0700

    dev: done UI

--- src/components/Translate.tsx
+++ src/components/Translate.tsx
@@ -8,11 +8,11 @@ export default function Translate() {
         <div className="flex gap-2 items-center">
 
             <button onClick={() => i18n.changeLanguage('en')}
-                    className="px-2 py-1 rounded bg-[rbg(var(--bg))] text-[rbg(var(--text))]">
+                    className="px-2 py-1 rounded bg-[rbg(var(--bg))] text-[rgb(var(--text))]">
                 🇬🇧 {!isMobile && 'English'}
             </button>
             <button onClick={() => i18n.changeLanguage('vi')}
-                    className="px-2 py-1 rounded bg-[rbg(var(--bg))] text-[rbg(var(--text))]">
+                    className="px-2 py-1 rounded bg-[rbg(var(--bg))] text-[rgb(var(--text))]">
                 🇻🇳 {!isMobile && 'Tiếng Việt'}
             </button>
         </div>
